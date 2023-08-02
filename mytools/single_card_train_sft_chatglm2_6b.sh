3dc28e3f46313e35c4f9fc59bbb0d3a77930fb6b
gLinxi <gzxmling@foxmail.com>
2023-08-02 14:39:28 +0800

    update script

--- mytools/single_card_train_sft_chatglm2_6b.sh
+++ mytools/single_card_train_sft_chatglm2_6b.sh
@@ -3,36 +3,42 @@
 # date
 date=`date +%Y%m%d%H`
 
-# params
-gpu_id=2
-model="intention_v1_chatglm26b"
-sft_data="intention_train"
-output_dir="./model/"${model}"_ckp"
-log_dir="./log/"${model}"_"${date}
-
-# default
-model_base_dir="/home/apps/gzx/LocalModelHub"
+# hub default path
+model_base_dir="/root/share/LocalModelHub"
+# model_base_dir="/home/apps/gzx/LocalModelHub"
 # model_base_dir="/data/jupyterlab/gzx/LocalModelHub/"
-chatglm2_6b=${model_base_dir}"/chatglm2_6b/hf"
-baichuan_7b=${model_base_dir}"/baichuan/hf"
+
+# models
+baichuan_13b_chat=${model_base_dir}"/baichuan_13b_chat/hf"
+
+# params
+gpu_id=0
+model="test_chatglm6b"
+sft_data="tool_v2_train"
+output_dir="${model_base_dir}/"${model}"/ckp"
+log_dir="${model_base_dir}/"${model}"/log"
+
+mkdir -p ${output_dir}
+mkdir -p ${log_dir}
 
 set -x
-CUDA_VISIBLE_DEVICES=${gpu_id} python ./src/train_sft.py \
-    --model_name_or_path ${baichuan_7b} \
+CUDA_VISIBLE_DEVICES=${gpu_id} python ./src/train_bash.py \
+    --model_name_or_path ${baichuan_13b_chat} \
+    --stage sft \
     --do_train \
-    --dataset  ${sft_data}\
+    --dataset  ${sft_data} \
     --finetuning_type lora \
     --output_dir ${output_dir} \
     --overwrite_cache \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 16 \
     --lr_scheduler_type cosine \
-    --logging_steps 10 \
-    --save_steps 1000 \
+    --logging_steps 1 \
+    --save_steps 3000 \
     --warmup_steps 0 \
     --learning_rate 1e-3 \
     --max_source_length 2048 \
     --max_target_length 2048 \
     --ddp_find_unused_parameters False \
     --num_train_epochs 3.0 \
-    --fp16 &>> ${log_dir}
+    --fp16 > ${log_dir}/${date} 2>&1
